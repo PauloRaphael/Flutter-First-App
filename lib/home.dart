e68e9a12b24ef4cc60480616c2b81b4597ce7b69
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-10-15 19:39:13 -0300

    First Project

---
+++ lib/home.dart
@@ -0,0 +1,45 @@
+import 'package:coffee_card/styled_body_text.dart';
+import 'package:flutter/material.dart';
+import 'package:coffee_card/coffee_prefs.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('My Coffee Id',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          backgroundColor: const Color(0xFF5D4037),
+          centerTitle: true,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.brown,
+              padding: const EdgeInsets.all(20),
+              child: const StyledBodyText('How i like my coffee...'),
+            ),
+            Container(
+              color: Colors.brown[100],
+              padding: const EdgeInsets.all(20),
+              child: const CoffeePrefs(),
+            ),
+            Expanded(
+              child: Image.asset('assets/img/coffee_bg.jpg',
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.bottomCenter,
+              )
+            ),
+
+          ],
+        ));
+  }
+}
+
+
+//Container widgets
+// --wraps other widgets
